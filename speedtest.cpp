c21e9427ffb7832df38b6195f346cd13df887379
chuckxcp <chuckxcp@163.com>
2014-02-25 14:53:08 +0800

    20140225 xucp add B+ tree speed test demo

---
+++ speedtest.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include "bplustree.h"
+
+#define BPTREE_ORDER 4
+#define BPTREE_KEY_LENGTH 8
+#define TOTAL_RECORD_NUM 500000
+
+int main(int argc, char ** argv)
+{
+	BPlusTree bptree(BPTREE_ORDER, BPTREE_KEY_LENGTH);
+
+	struct timeval start;
+	struct timeval end;
+
+	printf("Total record number: %d\n", TOTAL_RECORD_NUM);
+	int i = 0;
+	gettimeofday(&start, NULL);
+	for(i = 1; i <= TOTAL_RECORD_NUM; i++)
+	{
+		bptree.Insert(i, i);
+	}
+	gettimeofday(&end, NULL);
+	long costtime = 1000000*(end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec;
+	printf("Insert time: %ldus\n", costtime);
+
+	gettimeofday(&start, NULL);
+	for(i = 0; i < TOTAL_RECORD_NUM; i++)
+	{
+		int key = i+1;
+		record * rcd = bptree.Find(key, false);
+		if(rcd != NULL)
+		{
+			//printf("key: %d, value: %d\n", key, rcd->value);
+		}
+		else
+		{
+			printf("key: %d not found\n", key);
+		}
+	}
+	gettimeofday(&end, NULL);
+	costtime = 1000000*(end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec;
+	printf("Select time: %ldus\n", costtime);
+
+	return 0;
+}
+
+
